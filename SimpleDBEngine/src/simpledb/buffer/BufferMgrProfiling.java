43fd638d0de63e687c7fa32ea3739eed018b1944
Aryan Wadhwani <wadhwani@purdue.edu>
2021-10-01 15:44:55 -0400

    Added Update Table

--- SimpleDBEngine/src/simpledb/buffer/BufferMgrProfiling.java
+++ SimpleDBEngine/src/simpledb/buffer/BufferMgrProfiling.java
@@ -56,7 +56,8 @@ public class BufferMgrProfiling {
         BufferMgr bm = db.bufferMgr();
         bm.setMode(mode);
 
-        String s = "create table STUDENT(SId int, SFirstName varchar(40), SLastName varchar(40), MajorId int, GradYear int)";
+        String s = "create table STUDENT(SId int, SFirstName varchar(40), " +
+                "SLastName varchar(40), MajorId int, GradYear int)";
         stmt.executeUpdate(s);
         System.out.println("Table STUDENT created.");
 
@@ -103,7 +104,8 @@ public class BufferMgrProfiling {
 
     public static Result selectTableTest(int n, boolean mode) throws SQLException {
         EmbeddedDriver d = new EmbeddedDriver();
-        String url = "jdbc:simpledb:studentdb" + UUID.randomUUID().toString().substring(9); //Makes new database each time
+        String url = "jdbc:simpledb:studentdb" + UUID.randomUUID().toString().substring(9);
+        //Makes new database each time
         Connection conn = d.connect(url, null);
         Statement stmt = conn.createStatement();
 
@@ -112,7 +114,8 @@ public class BufferMgrProfiling {
         BufferMgr bm = db.bufferMgr();
         bm.setMode(mode);
 
-        String s = "create table STUDENT(SId int, SFirstName varchar(40), SLastName varchar(40), MajorId int, GradYear int)";
+        String s = "create table STUDENT(SId int, SFirstName varchar(40), " +
+                "SLastName varchar(40), MajorId int, GradYear int)";
         stmt.executeUpdate(s);
         System.out.println("Table STUDENT created.");
 
@@ -144,6 +147,124 @@ public class BufferMgrProfiling {
         return new Result(bm.hits, bm.misses);
     }
 
+    public static Result largeJoinTableTest(int n, boolean mode) throws SQLException {
+        EmbeddedDriver d = new EmbeddedDriver();
+        String url = "jdbc:simpledb:studentdb" + UUID.randomUUID().toString().substring(9); //Makes new database each time
+        Connection conn = d.connect(url, null);
+        Statement stmt = conn.createStatement();
+
+        // Debugging Setup
+        SimpleDB db = d.getDb();
+        BufferMgr bm = db.bufferMgr();
+        bm.setMode(mode);
+
+        String s = "create table STUDENT(SId int, SFirstName varchar(40), " +
+                "SLastName varchar(40), MajorId int, GradYear int)";
+        stmt.executeUpdate(s);
+        System.out.println("Table STUDENT created.");
+
+        ArrayList<Name> names = Name.generateNames(n);
+
+        s = "insert into STUDENT(SId, SFirstName, SLastName, MajorId, GradYear) values ";
+        String[] studvals = new String[n / 2];
+
+        for (int i = 0; i < n / 2; i++) {
+            studvals[i] = String.format("(%d, '%s', '%s', %d, %d)", (i + 1), names.get(i).firstName,
+                    names.get(i).lastName, rand.nextInt(courseNames.length), randomGradYear());
+        }
+        for (String studval : studvals) stmt.executeUpdate(s + studval);
+        System.out.println("STUDENT records inserted.");
+
+        s = "create table MAJOR(MId int, MajorName varchar(40), MajorAbbr varchar(5))";
+        stmt.executeUpdate(s);
+        System.out.println("Table MAJOR created.");
+
+        s = "insert into MAJOR(MId, MajorName, MajorAbbr) values ";
+        String[] majorvals = new String[courseNames.length];
+        for (int i = 0; i < courseNames.length; i++) {
+            majorvals[i] = String.format("(%d, '%s', '%s')", i, courseNames[i], courseAbs[i]);
+        }
+        for (String majorval : majorvals) stmt.executeUpdate(s + majorval);
+        System.out.println("MAJOR records inserted.");
+
+        s = "create table INSTRUCTOR(IId int, IFirstName varchar(40), " +
+                "ILastName varchar(40), DeptID int)";
+        stmt.executeUpdate(s);
+        System.out.println("Table INSTRUCTOR created.");
+
+        s = "insert into INSTRUCTOR(IId, IFirstName, ILastName, DeptID) values ";
+        String[] instrVals = new String[n / 2];
+
+        for (int i = n / 2; i < n; i++) {
+            instrVals[i - n / 2] = String.format("(%d, '%s', '%s', %d, %d)", (i + 1), names.get(i).firstName,
+                    names.get(i).lastName, rand.nextInt(courseNames.length), randomGradYear());
+        }
+        for (String instrVal : instrVals) stmt.executeUpdate(s + instrVal);
+        System.out.println("INSTRUCTOR records inserted.");
+
+        bm.hits = 0;
+        bm.misses = 0; // Resetting to only observe count during join
+
+        s = "select SId, SFirstName, SLastName, MId, MajorName, MajorAbbr, IID, IFirstName, ILastName, DeptID " +
+                "from STUDENT, MAJOR, INSTRUCTOR " +
+                "where MId = MajorId and DeptID = MId";
+
+        ResultSet rs = stmt.executeQuery(s);
+        int count = 0;
+        while (rs.next()) {
+            count++;
+        } // Going through entire result set.
+
+        conn.close();
+        return new Result(bm.hits, bm.misses);
+    }
+
+    public static Result updateTableTest(int n, boolean mode) throws SQLException {
+        EmbeddedDriver d = new EmbeddedDriver();
+        String url = "jdbc:simpledb:studentdb" + UUID.randomUUID().toString().substring(9);
+        //Makes new database each time
+        Connection conn = d.connect(url, null);
+        Statement stmt = conn.createStatement();
+
+        // Debugging Setup
+        SimpleDB db = d.getDb();
+        BufferMgr bm = db.bufferMgr();
+        bm.setMode(mode);
+
+        String s = "create table STUDENT(SId int, SFirstName varchar(40), " +
+                "SLastName varchar(40), MajorId int, GradYear int)";
+        stmt.executeUpdate(s);
+        System.out.println("Table STUDENT created.");
+
+        ArrayList<Name> names = Name.generateNames(n);
+
+        s = "insert into STUDENT(SId, SFirstName, SLastName, MajorId, GradYear) values ";
+        String[] studvals = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            studvals[i] = String.format("(%d, '%s', '%s', %d, %d)", (i + 1), names.get(i).firstName,
+                    names.get(i).lastName, rand.nextInt(courseNames.length), randomGradYear());
+        }
+        for (String studval : studvals) stmt.executeUpdate(s + studval);
+        System.out.println("STUDENT records inserted.");
+
+        bm.hits = 0;
+        bm.misses = 0; // Resetting to only observe count during join
+
+        s = "select SId, SFirstName, SLastName, MajorId " +
+                "from STUDENT";
+
+        for (int i = 0; i < n * 0.75; i++) {
+            String cmd = "update STUDENT "
+                    + "set MajorId=" + rand.nextInt(courseNames.length) + " "
+                    + "where SId = " + rand.nextInt(n) + 1;
+            stmt.executeUpdate(cmd);
+        }
+
+        conn.close();
+        return new Result(bm.hits, bm.misses);
+    }
+
     /**
      * Specifies size of experiment
      */
@@ -155,7 +276,7 @@ public class BufferMgrProfiling {
      * Performance of buffers is evaluated by hits and misses
      */
     public static void createTableTests() throws FileNotFoundException, SQLException {
-        PrintWriter pw = new PrintWriter(new File("createTable.csv"));
+        PrintWriter pw = new PrintWriter("createTable.csv");
         pw.println("mode,n,hits,misses,total");
         for (int n : nExperiments) {
             pw.println(createTableTest(n, true).toString("MRU", n));
@@ -169,7 +290,7 @@ public class BufferMgrProfiling {
      * Performance of buffers is evaluated by hits and misses
      */
     public static void joinTableTests() throws FileNotFoundException, SQLException {
-        PrintWriter pw = new PrintWriter(new File("joinTable.csv"));
+        PrintWriter pw = new PrintWriter("joinTable.csv");
         pw.println("mode,n,hits,misses,total");
         for (int n : nExperiments) {
             pw.println(joinTableTest(n, true).toString("MRU", n));
@@ -183,7 +304,7 @@ public class BufferMgrProfiling {
      * Performance of buffers is evaluated by hits and misses
      */
     public static void selectTableTests() throws FileNotFoundException, SQLException {
-        PrintWriter pw = new PrintWriter(new File("selectTable.csv"));
+        PrintWriter pw = new PrintWriter("selectTable.csv");
         pw.println("mode,n,hits,misses,total");
         for (int n : nExperiments) {
             pw.println(selectTableTest(n, true).toString("MRU", n));
@@ -192,11 +313,40 @@ public class BufferMgrProfiling {
         pw.close();
     }
 
+    /**
+     * Tests performance while joining two tables of various sizes, specified by nExperiments, with a constant size table.
+     * Performance of buffers is evaluated by hits and misses
+     */
+    public static void largeJoinTableTests() throws FileNotFoundException, SQLException {
+        PrintWriter pw = new PrintWriter("largeJoinTable.csv");
+        pw.println("mode,n,hits,misses,total");
+        for (int n : nExperiments) {
+            pw.println(largeJoinTableTest(n, true).toString("MRU", n));
+            pw.println(largeJoinTableTest(n, false).toString("default", n));
+        }
+        pw.close();
+    }
+
+    /**
+     * Tests performance while updating values in tables of various sizes
+     */
+    public static void updateTableTests() throws FileNotFoundException, SQLException {
+        PrintWriter pw = new PrintWriter("updateTable.csv");
+        pw.println("mode,n,hits,misses,total");
+        for (int n : nExperiments) {
+            pw.println(updateTableTest(n, true).toString("MRU", n));
+            pw.println(updateTableTest(n, false).toString("default", n));
+        }
+        pw.close();
+    }
+
     public static void main(String[] args) {
         try {
-//            createTableTests();
-//            joinTableTests();
+            createTableTests();
+            joinTableTests();
             selectTableTests();
+            largeJoinTableTests();
+            updateTableTests();
         } catch (SQLException | FileNotFoundException throwables) {
             throwables.printStackTrace();
         }
