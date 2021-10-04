dae2db194523e3c9e4da5d434118b5f8a0c6b9b7
Aryan Wadhwani <wadhwani@purdue.edu>
2021-10-04 11:39:26 -0400

    Formatting Tests

--- SimpleDBEngine/src/simpledb/buffer/BufferMgrTest.java
+++ SimpleDBEngine/src/simpledb/buffer/BufferMgrTest.java
@@ -6,18 +6,24 @@ import simpledb.file.*;
 public class BufferMgrTest {
 
     static void mruBasicTest() {
+
+        System.out.println("\n\n---- MRU BASIC TEST ----\n\n");
         SimpleDB db = new SimpleDB("buffermgrtest", 400, 5); // only 3 buffers
         BufferMgr bm = db.bufferMgr();
-        // bm.setMode(false);
+        bm.setMode(false);
+
         Buffer[] buff = new Buffer[5];
         for (int i = 0; i < 5; i++) {
             buff[i] = bm.pin(new BlockId("testfile", i + 1));
         }
         bm.unpin(buff[3]);
         bm.unpin(buff[2]);
-        System.out.println("Test 1\n");
         bm.pin(new BlockId("testfile", 21)); // Should go to buff[2]
         bm.pin(new BlockId("testfile", 30)); // Should go to buff[3]
+
+        assert buff[2].block().number() == 21;
+        assert buff[3].block().number() == 30;
+
         for (int i = 0; i < buff.length; i++) {
             Buffer b = buff[i];
             if (b != null)
@@ -28,7 +34,11 @@ public class BufferMgrTest {
         bm.unpin(buff[2]);
         bm.pin(new BlockId("testfile", 22)); // Should go to buff[3]
         bm.pin(new BlockId("testfile", 31)); // Should go to buff[2]
-        System.out.println("Test 2\n");
+
+        assert buff[2].block().number() == 31;
+        assert buff[3].block().number() == 22;
+
+        System.out.println();
         for (int i = 0; i < buff.length; i++) {
             Buffer b = buff[i];
             if (b != null)
@@ -37,6 +47,7 @@ public class BufferMgrTest {
     }
 
     static void basicTest() {
+        System.out.println("\n\n---- BASIC TEST ----\n\n");
         SimpleDB db = new SimpleDB("buffermgrtest", 400, 3); // only 3 buffers
         BufferMgr bm = db.bufferMgr();
         Buffer[] buff = new Buffer[6];
